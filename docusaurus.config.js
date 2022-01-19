98dc4c148374e457e3d9fdb000cbb5359489e86f
Brian R. Bondy <netzen@gmail.com>
2022-01-19 16:15:03 -0500

    Fix copyright text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Brave Software, Inc.`,
       },
       prism: {
         theme: lightCodeTheme,
