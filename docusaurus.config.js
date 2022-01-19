3491895cc9340ac5ae2ac79eb902f27211f82c5f
Brian R. Bondy <netzen@gmail.com>
2022-01-19 16:34:51 -0500

    Fix favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'brave', // Usually your GitHub org/user name.
   projectName: 'brave-wallet-docs', // Usually your repo name.
 
