4afac4c00acaa12bf1b36cb1215f6e53dc0e356b
Josh Leonard <30185185+josheleonard@users.noreply.github.com>
2023-11-15 13:16:17 +0000

    chore: use single-quotes for js files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,29 +3,29 @@
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Brave Wallet",
-  tagline: "",
-  url: "https://wallet-docs.brave.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.png",
-  organizationName: "brave", // Usually your GitHub org/user name.
-  projectName: "brave-wallet-docs", // Usually your repo name.
+  title: 'Brave Wallet',
+  tagline: '',
+  url: 'https://wallet-docs.brave.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  organizationName: 'brave', // Usually your GitHub org/user name.
+  projectName: 'brave-wallet-docs', // Usually your repo name.
 
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: "https://github.com/brave/brave-wallet-docs/edit/main/",
+          editUrl: 'https://github.com/brave/brave-wallet-docs/edit/main/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -35,10 +35,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Brave Wallet",
+        title: 'Brave Wallet',
         logo: {
-          alt: "Brave Wallet Logo",
-          src: "img/brave128.png",
+          alt: 'Brave Wallet Logo',
+          src: 'img/brave128.png',
         },
         items: [
           /*
@@ -50,58 +50,58 @@ const config = {
           },
           */
           {
-            to: "https://brave.com/download",
-            label: "Download",
-            position: "left",
+            to: 'https://brave.com/download',
+            label: 'Download',
+            position: 'left',
           },
           {
-            to: "https://twitter.com/brave",
-            label: "Twitter",
-            position: "left",
+            to: 'https://twitter.com/brave',
+            label: 'Twitter',
+            position: 'left',
           },
           {
-            to: "https://github.com/brave/brave-browser",
-            label: "Github",
-            position: "left",
+            to: 'https://github.com/brave/brave-browser',
+            label: 'Github',
+            position: 'left',
           },
           {
-            href: "https://github.com/brave/brave-wallet-docs",
-            label: "Docs GitHub",
-            position: "right",
+            href: 'https://github.com/brave/brave-wallet-docs',
+            label: 'Docs GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Documentation",
-                to: "/",
+                label: 'Documentation',
+                to: '/',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/brave",
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/brave',
               },
               {
-                label: "Twitter",
-                href: "https://twitter.com/brave",
+                label: 'Twitter',
+                href: 'https://twitter.com/brave',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "GitHub",
-                href: "https://github.com/brave/brave-browser",
+                label: 'GitHub',
+                href: 'https://github.com/brave/brave-browser',
               },
             ],
           },
